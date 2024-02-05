97e46c5b6c06d8f37360fec5a1f7e6e15faa43aa
widath2 <widath2222@gmail.com>
2024-02-05 21:13:32 +0530

    final commit

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/dashboard', function () {
+    return view('layouts.app');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+ Route::resource(
+    'product-category',
+    \App\Http\Controllers\ProductCategoryController::class
+ );
+
+Route::resource(
+    'user',
+    \App\Http\Controllers\UserController::class
+);
